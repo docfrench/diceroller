92bb9868d1e4484d37a46b993f461ef69fb1c486
Brian French <frenchbm@gmail.com>
2026-09-20 14:59:33 -0400

    troubleshooting env still

--- diceroller.go
+++ diceroller.go
@@ -147,6 +147,7 @@ func main() {
 	}()
 
 	fmt.Println("Dice Roller is listening on port 8080")
+	fmt.Println("Enter passphrase:", tablePassphrase)
 	http.HandleFunc("/", HomePage)
 	http.HandleFunc("/roll", RollHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
